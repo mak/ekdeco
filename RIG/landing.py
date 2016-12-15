5c91324727a0484d2e795e45851dfb3ca70c873e
mak <mak@cert.pl>
2016-12-15 13:25:14 +0100

    RIG: fix rig-e extraction

--- RIG/landing.py
+++ RIG/landing.py
@@ -12,7 +12,7 @@ apr.add_argument('-o','--out',help="prefix for filename used to store decoded ch
 args = None
 
 def clear_comments(d):
-    return re.sub('/\*[^/]*\*/','',d)
+    return re.sub('/\*[^/]*\*/','',d).replace('"+"','')
 
 def decode_fdata(data):
     alphabet = 'za1sd0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'[5:]
@@ -28,16 +28,29 @@ def write_payload(t,payload):
         with open(args.dir + os.sep + '.'.join([args.out,h,t]),'w') as f:
             f.write(payload)
 
+def extract_from_swfpage(d):
+    r ={}
+    data = d.split('"')
+    r['flash_url'] = data[-6]
+    r['flash_drop_1'] = data[-4]
+    r['drop_key'] = data[-2]
+    return r
 
+            
 def do_rige(d):
     r = {'vb_drops':[]}
     for s in re.findall('<script>(.+?)</script>',d,re.S):
 #        if not s: continue
         
         d = clear_comments(s)
-        basep,_,g,_=re.findall('"([/A-Za-z0-9=+]+)"\.replace\((/|"|\')([^/"\']+)("|/|\')',d)[0]
+	xxx = re.findall('"([/A-Za-z0-9=+]+)"\.replace\((/|"|\')([^/"\']+)("|/|\')',d)
+	if xxx:
+            basep,_,g,_= xxx[0]
+        else:
+	    ## last month changes...
+	    basep = re.findall('"([/A-Za-z0-9=+]{65,})";',d)[0]
+	    g= ''
         payload = clear_comments(basep.replace(g,'').decode('base64'))
-        
         if 'Dim' in payload:
             x='http'+re.findall('"http(.*?)"',payload)[0]
             r['vb_drops'].append(x)
@@ -46,9 +59,12 @@ def do_rige(d):
             d=payload.split(';')[-2].split('"')
             flash_url = d[1]
             r['flash_url'] = flash_url
-            id,key,url = decode_fdata(d[-2]).split(';')
-            r['flash_drop_%s'%id] = url
-            r['drop_key'] = key
+	    try:
+                id,key,url = decode_fdata(d[-2]).split(';')		        
+	        r['flash_drop_%s'%id] = url
+                r['drop_key'] = key
+            except:
+                r.update(extract_from_swfpage(payload))
             t = 'flash_drop.html'
         else:
             t = 'exploit.js'
@@ -79,10 +95,7 @@ def do_rigv(d):
             t= 'exploit.vbs'
         
         elif 'application/x-shockwave-flash' in payload:
-            data = payload.split('"')
-            r['flash_url'] = data[-6]
-            r['flash_drop_1'] = data[-4]
-            r['drop_key'] = data[-2]
+            r.update(extract_from_swfpage(payload))
             t = 'flash_drop.html'
 
         else:
@@ -97,6 +110,7 @@ def doit(d):
     try:
         r = do_rige(d)
     except Exception as e:
+	print `e`
         r=do_rigv(d)
     return r
 
